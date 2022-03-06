ccd8dd73e7dfdb671314b76ef66827d5a761a4db
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-03-06 16:57:54 +0700

    Rewrite and correct function of starttable

--- main.c
+++ main.c
@@ -4,38 +4,36 @@
 #define Weight 8
 #define Height 8
 
-void starttable(int tab[Weight][Height])
-{
-    for (int i = 0; i < 8; i++) {
-        switch (i) {
-        case 0:
-        case 7:
-            tab[i][0] = 2;
-            tab[i][1] = 3;
-            tab[i][2] = 4;
-            tab[i][3] = 5;
-            tab[i][4] = 6;
-            tab[i][5] = 4;
-            tab[i][6] = 3;
-            tab[i][7] = 2;
-            break;
-        case 1:
-        case 6:
-            for (int j = 0; j < 8; j++) {
-                tab[i][j] = 1;
-            }
-            break;
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        default:
-            for (int j = 0; j < 8; j++) {
-                tab[i][j] = 0;
-            }
-            break;
-        }
-    }
+void starttable(int tab[Weight][Height]){
+	for(int i=0;i<8;i++){
+		switch(i){
+			case 0:
+				tab[i][0]=2; tab[i][1]=3; tab[i][2]=4;
+				tab[i][3]=5; tab[i][4]=6; tab[i][5]=4;
+				tab[i][6]=3; tab[i][7]=2;
+				break;
+			case 7:
+				tab[i][0]=20; tab[i][1]=30; tab[i][2]=40;
+				tab[i][3]=50; tab[i][4]=60; tab[i][5]=40;
+				tab[i][6]=30; tab[i][7]=20;
+				break;
+			case 1:
+				for(int j=0;j<8;j++){
+					tab[i][j]=1;
+				}
+				break;
+			case 6:
+				for(int j=0;j<8;j++){
+					tab[i][j]=10;
+				}
+				break;
+			case 2: case 3: case 4: case 5: default:
+				for(int j=0;j<8;j++){
+					tab[i][j]=0;
+				}
+				break;
+		}
+	}
 }
 
 void printtable(int tab[Weight][Height])
