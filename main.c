34fee8b6382e79ee15110377a77769307d501f3a
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-03-06 17:05:34 +0700

    Rewrite main.c without clangformat

--- main.c
+++ main.c
@@ -101,9 +101,40 @@ void erormes(int *er, FILE *ofile){
 
 int main()
 {
-    int tab[Weight][Height];
-    starttable(tab);
-    printtable(tab);
-    return 0;
+    int x=8, y=8, ex=0, c=0, er=0, hod=1;
+	int tab[Weight][Height];
+	char line[20];
+	
+	FILE *file = fopen("text.txt","r");
+	if(file==NULL){
+		printf("Error. Text file do not opened. Check text.txt file.");
+		return -3;
+	}
+	
+	FILE *ofile = fopen("output.txt","w");
+	if(ofile==NULL){
+		printf(" Errore. Output file do not opened. Check output.txt file. ");
+		return -3;
+	}
+	
+	starttable(tab); printtable(0,line,0,0,tab,ofile);
+	printf("\n \n"); fprintf(ofile,"\n \n");
+	while(ex<2){
+		while((c=fgetc(file)) != EOF){ 
+			fgets(line,20,file); 
+			move(hod,line,tab,&er,ofile); 
+			printf("\n \n");
+			fprintf(ofile,"\n \n");
+			hod++;
+			if(er>0)break;
+		}
+		erormes(&er,ofile);
+		if(er>0) {
+			return -3;
+		}
+		ex+=20;
+	}
+	fclose(file); fclose(ofile);
+	return 0;
 }
 
