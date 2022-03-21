255206846b5cf4d57161e4d659bd93bd2c350853
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-03-21 18:38:15 +0700

    funtcion for erors

---
+++ src/libchessviz/eror_mes.c
@@ -0,0 +1,43 @@
+#include <libchessviz/eror_mes.h>
+
+void erormes(int* er, FILE* ofile)
+{
+    switch (*er) {
+    case 1:
+        printf(" \nViolation of the rules for the movement of figures.\n ");
+        fprintf(ofile,
+                " \nViolation of the rules for the movement of figures.\n ");
+        break;
+    case 2:
+        printf(" \nThe piece that was supposed to move was not in "
+               "position.\n ");
+        fprintf(ofile,
+                " \nThe piece that was supposed to move was not in "
+                "position.\n ");
+        break;
+    case 3:
+        printf(" \nWith a quiet move, a figure appeared on the way.\n ");
+        fprintf(ofile,
+                " \nWith a quiet move, a figure appeared on the way.\n ");
+        break;
+    case 4:
+        printf(" \nWhen capturing, there was no piece to capture.\n ");
+        fprintf(ofile, " \nWhen capturing, there was no piece to capture.\n ");
+        break;
+    case 5:
+        printf(" \nExceeding the field boundaries or non-existent "
+               "position.\n ");
+        fprintf(ofile,
+                " \nExceeding the field boundaries or non-existent "
+                "position.\n ");
+        break;
+    case 6:
+        printf(" \nThe sequence of moves is broken.\n ");
+        fprintf(ofile, " \nThe sequence of moves is broken.\n ");
+        break;
+    default:
+        printf(" \nThe party is over. There are no errors.\n ");
+        fprintf(ofile, " \nThe party is over. There are no errors.\n ");
+        break;
+    }
+}
