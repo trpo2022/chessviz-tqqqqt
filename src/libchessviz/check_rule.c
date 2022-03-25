3fdfad3e2fcf53b4ba0b4d4026ea7f0c2b442723
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-03-25 19:35:15 +0700

    Changed obscure names

--- src/libchessviz/check_rule.c
+++ src/libchessviz/check_rule.c
@@ -1,58 +1,58 @@
 #include <libchessviz/check_rule.h>
 
-void checkrule(
-        int hod, int x, int y, int x1, int y1, int flag, int fig, int** er)
+void check_rule(
+        int step, int x, int y, int x1, int y1, int flag, int figures, int** error)
 {
-    switch (fig) {
+    switch (figures) {
     case 1:
     case 10: // peshka
         if ((abs(y1 - y) > 2 || abs(x1 - x) > 0) && flag == 1)
-            **er = 1;
+            **error = 1;
         if ((abs(x1 - x) > 1 || abs(y1 - y) > 1) && flag == 2)
-            **er = 1;
+            **error = 1;
         if (abs(x1 - x) > 1 && (y != 7 || y != 1))
-            **er = 1;
+            **error = 1;
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         break;
     case 2:
     case 20: // ladya
         if (abs(x1 - x) > 0 && abs(y1 - y) > 0)
-            **er = 1;
+            **error = 1;
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         break;
     case 3:
     case 30: // koni
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         if ((abs(y1 - y) == 2 && abs(x1 - x) == 1)
             || (abs(y1 - y) == 1 && abs(x1 - x) == 2)) {
         } else
-            **er = 1;
+            **error = 1;
         break;
     case 4:
     case 40: // slon
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         if ((abs(x1 - x) > 0 && abs(y1 - y) == 0)
             || ((y1 - y) > 0 && (x1 - x) == 0))
-            **er = 1;
+            **error = 1;
         break;
     case 5:
     case 50: // ferz
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         if ((abs(x1 - x) == 2 && abs(y1 - y) == 1)
             || (abs(x1 - x) == 1 && abs(y1 - y) == 2))
-            **er = 1;
+            **error = 1;
         break;
     case 6:
     case 60: // korol
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **er = 5;
+            **error = 5;
         if (abs(x1 - x) > 1 && abs(y1 - y) > 1)
-            **er = 1;
+            **error = 1;
         break;
     }
 }
