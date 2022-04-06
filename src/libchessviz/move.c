2095a9c99dae54f8f6c37ac14fd024efc2201b5e
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-06 17:12:18 +0700

    Correct old code

--- src/libchessviz/move.c
+++ src/libchessviz/move.c
@@ -8,94 +8,105 @@ void move_figure(
         int yy,
         int xx1,
         int yy1,
-        int move_flag,
+        int* move_flag,
         int first_sign,
+int last_sign,
         char text[size],
         // int print_flag,
         int end_of_record,
         int need_figure,
-        int symbol,
+int* player_turn,
+        //int symbol,
         int table[Weight][Height],
         int* error,
         FILE* ofile)
 {
-    int last_sign = 0;
-    int memory = 0, players_turn = 0;
-    //while (*error == 0) {
-        if (end_of_record == 1 && move_flag == 1) {
-            last_sign = symbol;
+    //int last_sign = 0;
+    int memory = 0; //players_turn = 0;
+    while (*error == 0) {
+        if (end_of_record == 1 && *move_flag == 1) {
+           // last_sign = symbol;
             if (need_figure > 0
                 && (table[yy][xx] != need_figure
                     && table[yy][xx] != (need_figure / 10))) {
                 *error = 2;
+break;
             }
             if (table[yy1][xx1] != 0) {
                 *error = 3;
+break;
             }
             check_rule(
                     xx,
                     yy,
                     xx1,
                     yy1,
-                    move_flag,
+                    *move_flag,
                     table[yy][xx],
                     table[yy1][xx1],
                     &error);
+if(*error>0) break;
             memory = table[yy][xx];
             table[yy][xx] = 0;
             table[yy1][xx1] = memory;
-            move_flag = 3;
+            *move_flag = 3;
             print_table(step, text, first_sign, last_sign, table, ofile);
             if (table[yy1][xx1] <= 6
-                && (players_turn == 0 || players_turn == 2))
-                players_turn = 1;
+                && (*player_turn == 0 || *player_turn == 2))
+                *player_turn = 1;
             else if (
                     table[yy1][xx1] >= 10 && table[yy1][xx1] <= 60
-                    && players_turn == 1)
-                players_turn = 2;
+                    && *player_turn == 1)
+                *player_turn = 2;
             else
                 *error = 6;
+break;
         }
-        if (end_of_record == 1 && move_flag == 2) {
-            last_sign = symbol;
+
+        if (end_of_record == 1 && *move_flag == 2) {
+           // last_sign = symbol;
             if (need_figure > 0
                 && (table[yy][xx] != need_figure
                     && table[yy][xx] != (need_figure / 10))) {
                 *error = 2;
+break;
             }
             if (table[yy1][xx1] == 0) {
                 *error = 4;
+break;
             }
             check_rule(
                     xx,
                     yy,
                     xx1,
                     yy1,
-                    move_flag,
+                    *move_flag,
                     table[yy][xx],
                     table[yy1][xx1],
                     &error);
+if(*error>0) break;
             memory = table[yy][xx];
             table[yy][xx] = 0;
             table[yy1][xx1] = memory;
-            move_flag = 3;
+            *move_flag = 3;
             print_table(step, text, first_sign, last_sign, table, ofile);
             if (table[yy1][xx1] <= 6
-                && (players_turn == 0 || players_turn == 2))
-                players_turn = 1;
+                && (*player_turn == 0 || *player_turn == 2))
+                *player_turn = 1;
             else if (
                     table[yy1][xx1] >= 10 && table[yy1][xx1] <= 60
-                    && players_turn == 1)
-                players_turn = 2;
+                    && *player_turn == 1)
+                *player_turn = 2;
             else
                 *error = 6;
+break;
         }
 
-        if (text[symbol] == 35 || text[symbol] == '+') {
-        }
+        //if (text[symbol] == 35 || text[symbol] == '+') {
+        //}
 
-        if (move_flag == 3) {
-            move_flag = 0;
+        if (*move_flag == 3) {
+            *move_flag = 0;
             end_of_record = 0;
             // print_flag = 0;
             need_figure = 0;
@@ -105,7 +116,7 @@ void move_figure(
             xx1 = 0;
             yy1 = 0;
         }
-   // }
+    }
     /*if (*error > 0) {
         break;
     }*/
