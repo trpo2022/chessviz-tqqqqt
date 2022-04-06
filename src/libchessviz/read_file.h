ccdd84f7a5d859a04df20eef9a27736af37a2afb
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-06 16:30:39 +0700

    Added lib for c code

---
+++ src/libchessviz/read_file.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define Weight 8
+#define Height 8
+#define reading_artifacts 2
+#define size 40
+
+void read_file(int step, char text[], FILE* ofile, int* error, int table[Weight][Height]);
