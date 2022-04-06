97a19dc6ea9f315e389cb2a7a6dc720afd539fd4
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-06 17:31:20 +0700

    Rewrite function and options for function

--- src/libchessviz/move.c
+++ src/libchessviz/move.c
@@ -10,31 +10,27 @@ void move_figure(
         int yy1,
         int* move_flag,
         int first_sign,
-int last_sign,
+        int last_sign,
         char text[size],
-        // int print_flag,
         int end_of_record,
         int need_figure,
-int* player_turn,
-        //int symbol,
+        int* player_turn,
         int table[Weight][Height],
         int* error,
         FILE* ofile)
 {
-    //int last_sign = 0;
-    int memory = 0; //players_turn = 0;
+    int memory = 0;
     while (*error == 0) {
         if (end_of_record == 1 && *move_flag == 1) {
-           // last_sign = symbol;
             if (need_figure > 0
                 && (table[yy][xx] != need_figure
                     && table[yy][xx] != (need_figure / 10))) {
                 *error = 2;
-break;
+                break;
             }
             if (table[yy1][xx1] != 0) {
                 *error = 3;
-break;
+                break;
             }
             check_rule(
                     xx,
@@ -45,7 +41,8 @@ break;
                     table[yy][xx],
                     table[yy1][xx1],
                     &error);
-if(*error>0) break;
+            if (*error > 0)
+                break;
             memory = table[yy][xx];
             table[yy][xx] = 0;
             table[yy1][xx1] = memory;
@@ -60,20 +57,19 @@ if(*error>0) break;
                 *player_turn = 2;
             else
                 *error = 6;
-break;
+            break;
         }
 
         if (end_of_record == 1 && *move_flag == 2) {
-           // last_sign = symbol;
             if (need_figure > 0
                 && (table[yy][xx] != need_figure
                     && table[yy][xx] != (need_figure / 10))) {
                 *error = 2;
-break;
+                break;
             }
             if (table[yy1][xx1] == 0) {
                 *error = 4;
-break;
+                break;
             }
             check_rule(
                     xx,
@@ -84,7 +80,8 @@ break;
                     table[yy][xx],
                     table[yy1][xx1],
                     &error);
-if(*error>0) break;
+            if (*error > 0)
+                break;
             memory = table[yy][xx];
             table[yy][xx] = 0;
             table[yy1][xx1] = memory;
@@ -99,16 +96,12 @@ if(*error>0) break;
                 *player_turn = 2;
             else
                 *error = 6;
-break;
+            break;
         }
-
-        //if (text[symbol] == 35 || text[symbol] == '+') {
-        //}
 
         if (*move_flag == 3) {
             *move_flag = 0;
             end_of_record = 0;
-            // print_flag = 0;
             need_figure = 0;
             xx = 0;
             yy = 0;
@@ -116,9 +109,8 @@ break;
             xx1 = 0;
             yy1 = 0;
         }
+        if (*error > 0) {
+            break;
+        }
     }
-    /*if (*error > 0) {
-        break;
-    }*/
 }
-
