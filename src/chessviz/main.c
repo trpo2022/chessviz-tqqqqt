4c63db2ab3116ae3f142d313f4556d5628c80f1f
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-13 19:27:47 +0700

    Delete enter in read and added new body for chess

--- src/chessviz/main.c
+++ src/chessviz/main.c
@@ -1,23 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <chessviz/start_table.h>
-#include <libchessviz/check_rule.h>
-#include <libchessviz/eror_mes.h>
-#include <libchessviz/move.h>
-#include <libchessviz/print_table.h>
-#include <libchessviz/read_file.h>
-
-#define Weight 8
-#define Height 8
-#define size 40
+#include <chessviz/main_body.h>
 
 int main()
 {
-    int exit_status = 0, end_of_line = 0, error = 0, step = 1;
-    int table[Weight][Height];
-    char line[size];
-
     FILE* file = fopen("text.txt", "r");
     if (file == NULL) {
         printf("Error. Text file do not opened. Check text.txt file.");
@@ -38,26 +25,8 @@ int main()
                 ofile = fopen("output.txt", "w");
     }
 
-    start_table(table);
-    print_table(0, line, 0, 0, table, ofile);
-    printf("\n \n");
-    fprintf(ofile, "\n \n");
-    while (exit_status < 2) {
-        while ((end_of_line = fgetc(file)) != EOF) {
-            fgets(line, size, file);
-            read_file(step, line, ofile, &error, table);
-            printf("\n \n");
-            fprintf(ofile, "\n \n");
-            step++;
-            if (error > 0)
-                break;
-        }
-        eror_message(&error, ofile);
-        if (error > 0) {
-            return 44;
-        }
-        exit_status += 20;
-    }
+    main_body(file, ofile);
+
     fclose(file);
     fclose(ofile);
     return 0;
