069cc55ab66f3dba9aeb1c1a0f09052253443ca3
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-13 20:40:49 +0700

    Rewrite error names

--- src/libchessviz/check_rule.h
+++ src/libchessviz/check_rule.h
@@ -5,6 +5,14 @@
 
 #define Weight 8
 #define Height 8
+#define Wait_mode 0
+#define Party_over 1
+#define Violation_of_rules 2
+#define Piece_not_in_pos 3
+#define Error_quite_move 4
+#define Error_capture_move 5
+#define Exceeding_field_bound 6
+#define Broken_sequence_moves 7
 
 void check_rule(
         int x,
