aa8b53636ff163d79759cf3619609b87b7d8a675
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-13 20:41:30 +0700

    Full rewrite function and and add enum and i'm sick

--- src/libchessviz/eror_mes.c
+++ src/libchessviz/eror_mes.c
@@ -1,43 +1,12 @@
 #include <libchessviz/eror_mes.h>
 
-void eror_message(int* error, FILE* ofile)
+void eror_message(char* ErrorNam[], int* error, FILE* ofile)
 {
-    switch (*error) {
-    case 1:
-        printf(" \nViolation of the rules for the movement of figures.\n ");
-        fprintf(ofile,
-                " \nViolation of the rules for the movement of figures.\n ");
-        break;
-    case 2:
-        printf(" \nThe piece that was supposed to move was not in "
-               "position.\n ");
-        fprintf(ofile,
-                " \nThe piece that was supposed to move was not in "
-                "position.\n ");
-        break;
-    case 3:
-        printf(" \nWith a quiet move, a figure appeared on the way.\n ");
-        fprintf(ofile,
-                " \nWith a quiet move, a figure appeared on the way.\n ");
-        break;
-    case 4:
-        printf(" \nWhen capturing, there was no piece to capture.\n ");
-        fprintf(ofile, " \nWhen capturing, there was no piece to capture.\n ");
-        break;
-    case 5:
-        printf(" \nExceeding the field boundaries or non-existent "
-               "position.\n ");
-        fprintf(ofile,
-                " \nExceeding the field boundaries or non-existent "
-                "position.\n ");
-        break;
-    case 6:
-        printf(" \nThe sequence of moves is broken.\n ");
-        fprintf(ofile, " \nThe sequence of moves is broken.\n ");
-        break;
-    default:
-        printf(" \nThe party is over. There are no errors.\n ");
-        fprintf(ofile, " \nThe party is over. There are no errors.\n ");
-        break;
+    if (*error == Wait_mode || *error == Party_over) {
+        puts(ErrorNam[*error]);
+        fputs(ErrorNam[*error], ofile);
+    } else {
+        puts(ErrorNam[*error - 1]);
+        fputs(ErrorNam[*error - 1], ofile);
     }
 }
