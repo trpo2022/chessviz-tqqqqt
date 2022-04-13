83bb205142a918be15360c88887c727bab7d20ba
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-13 20:40:15 +0700

    Added funy enum and char for error

--- src/chessviz/main_body.c
+++ src/chessviz/main_body.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #include <chessviz/start_table.h>
 #include <libchessviz/check_rule.h>
 #include <libchessviz/eror_mes.h>
@@ -5,12 +8,32 @@
 #include <libchessviz/print_table.h>
 #include <libchessviz/read_file.h>
 
+#define Wait_mode 0
+#define Party_over 1
+#define Violation_of_rules 2
+#define Piece_not_in_pos 3
+#define Error_quite_move 4
+#define Error_capture_move 5
+#define Exceeding_field_bound 6
+#define Broken_sequence_moves 7
+
+static char* ErrorNam[]
+        = {"The party is over. There are no errors.",
+           "Violation of the rules for the movement of figures.",
+           "The piece that was supposed to move was not in position.",
+           "With a quiet move, a figure appeared on the way.",
+           "When capturing, there was no piece to capture.",
+           "Exceeding the field boundaries or non-existent position.",
+           "The sequence of moves is broken."};
+
 void main_body(FILE* file, FILE* ofile)
 {
     // main
-    int exit_status = 0, end_of_line = 0, error = 0, step = 1;
+    int exit_status = 0, end_of_line = 0, step = 1;
     int table[Weight][Height];
     char line[size];
+    int error = Wait_mode;
+    error = Wait_mode;
 
     // check_rule
     // int figure_flag = 0, enemy_flag = 0, side_max_value = 10;
@@ -87,10 +110,10 @@ void main_body(FILE* file, FILE* ofile)
             printf("\n \n");
             fprintf(ofile, "\n \n");
             step++;
-            if (error > 0)
+            if (error > Wait_mode)
                 break;
         }
-        eror_message(&error, ofile);
+        eror_message(ErrorNam, &error, ofile);
         exit_status += 20;
     }
 }
