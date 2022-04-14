e810dc5b96ebbd5aa6f53deeb80edba6dc615788
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-14 15:56:12 +0700

    Rewrite some options and funct

--- src/libchessviz/check_rule.c
+++ src/libchessviz/check_rule.c
@@ -5,10 +5,10 @@ void check_rule(
         int y,
         int x1,
         int y1,
-        int** flag,
+        int* flag,
         int figures,
         int enemy,
-        int** error)
+        int* error)
 {
     int figure_flag = 0, enemy_flag = 0, side_max_value = 10;
 
@@ -25,65 +25,65 @@ void check_rule(
     switch (figures) {
     case 1:
     case 10: // peshka
-        if ((abs(y1 - y) > 2 || abs(x1 - x) > 0) && **flag == 1)
-            **error = Violation_of_rules;
+        if ((abs(y1 - y) > 2 || abs(x1 - x) > 0) && *flag == 1)
+            *error = Violation_of_rules;
         if ((abs(x1 - x) > 1 || abs(y1 - y) > 1 || figure_flag == enemy_flag)
-            && **flag == 2)
-            **error = Violation_of_rules;
+            && *flag == 2)
+            *error = Violation_of_rules;
         if ((abs(x1 - x) > 1 && (y != 6 && y != 1) && abs(y1 - y) > 0)
-            && **flag == 1)
-            **error = Violation_of_rules;
+            && *flag == 1)
+            *error = Violation_of_rules;
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
+            *error = Exceeding_field_bound;
         break;
     case 2:
     case 20: // ladya
         if (abs(x1 - x) > 0 && abs(y1 - y) > 0)
-            **error = Violation_of_rules;
+            *error = Violation_of_rules;
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
-        if (**flag == 2 && (figure_flag == enemy_flag))
-            **error = Violation_of_rules;
+            *error = Exceeding_field_bound;
+        if (*flag == 2 && (figure_flag == enemy_flag))
+            *error = Violation_of_rules;
         break;
     case 3:
     case 30: // koni
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
+            *error = Exceeding_field_bound;
         if ((abs(y1 - y) == 2 && abs(x1 - x) == 1)
             || (abs(y1 - y) == 1 && abs(x1 - x) == 2)) {
         } else
-            **error = Violation_of_rules;
-        if (**flag == 2 && (figure_flag == enemy_flag))
-            **error = Violation_of_rules;
+            *error = Violation_of_rules;
+        if (*flag == 2 && (figure_flag == enemy_flag))
+            *error = Violation_of_rules;
         break;
     case 4:
     case 40: // slon
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
+            *error = Exceeding_field_bound;
         if ((abs(x1 - x) > 0 && abs(y1 - y) == 0)
             || ((y1 - y) > 0 && (x1 - x) == 0))
-            **error = Violation_of_rules;
-        if (**flag == 2 && (figure_flag == enemy_flag))
-            **error = Violation_of_rules;
+            *error = Violation_of_rules;
+        if (*flag == 2 && (figure_flag == enemy_flag))
+            *error = Violation_of_rules;
         break;
     case 5:
     case 50: // ferz
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
+            *error = Exceeding_field_bound;
         if ((abs(x1 - x) == 2 && abs(y1 - y) == 1)
             || (abs(x1 - x) == 1 && abs(y1 - y) == 2))
-            **error = Violation_of_rules;
-        if (**flag == 2 && (figure_flag == enemy_flag))
-            **error = Violation_of_rules;
+            *error = Violation_of_rules;
+        if (*flag == 2 && (figure_flag == enemy_flag))
+            *error = Violation_of_rules;
         break;
     case 6:
     case 60: // korol
         if (abs(x1 - x) > Height || abs(y1 - y) > Weight)
-            **error = Exceeding_field_bound;
+            *error = Exceeding_field_bound;
         if (abs(x1 - x) > 1 && abs(y1 - y) > 1)
-            **error = Violation_of_rules;
-        if (**flag == 2 && (figure_flag == enemy_flag))
-            **error = Violation_of_rules;
+            *error = Violation_of_rules;
+        if (*flag == 2 && (figure_flag == enemy_flag))
+            *error = Violation_of_rules;
         break;
     }
 }
