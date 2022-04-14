e876b31a26b9fbe439fe9dfcf6b8b911be335ba6
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-14 15:57:36 +0700

    Full rewrite tests

--- test/test.c
+++ test/test.c
@@ -1,7 +1,12 @@
 #include <ctest.h>
 #include <stdio.h>
 
-CTEST(read_tt, simple_read)
+#include <libchessviz/check_rule.h>
+#include <libchessviz/read_file.h>
+#include <libchessviz/start_table.h>
+#include <libchessviz/move.h>
+
+CTEST(read_txt, simple_read_file)
 {
     FILE* file = fopen("test.txt", "r");
     char result[6];
@@ -13,84 +18,29 @@ CTEST(read_tt, simple_read)
     ASSERT_STR(result, expected);
 }
 
-CTEST(read_tt, sign_if_read)
+CTEST(read_sign, sign_translate_xy_cord)
 {
     FILE* file = fopen("test.txt", "r");
-    char result[6];
-    fgets(result, 6, file);
+    char result[8];
+    fgets(result, 8, file);
     fclose(file);
-
-    int xx = 0, yy = 0, flag = 0, xx1 = 0, yy1 = 0;
-    for (int symbol = 0; symbol <= 5; symbol++) {
-        if (result[symbol] == '-')
-            flag = 1;
-        if (result[symbol] == 'a' && flag == 0)
-            xx = 0;
-        else if (result[symbol] == 'b' && flag == 0)
-            xx = 1;
-        else if (result[symbol] == 'c' && flag == 0)
-            xx = 2;
-        else if (result[symbol] == 'd' && flag == 0)
-            xx = 3;
-        else if (result[symbol] == 'e' && flag == 0)
-            xx = 4;
-        else if (result[symbol] == 'f' && flag == 0)
-            xx = 5;
-        else if (result[symbol] == 'g' && flag == 0)
-            xx = 6;
-        else if (result[symbol] == 'h' && flag == 0)
-            xx = 7;
-        else if (result[symbol] == '1' && flag == 0)
-            yy = 0;
-        else if (result[symbol] == '2' && flag == 0)
-            yy = 1;
-        else if (result[symbol] == '3' && flag == 0)
-            yy = 2;
-        else if (result[symbol] == '4' && flag == 0)
-            yy = 3;
-        else if (result[symbol] == '5' && flag == 0)
-            yy = 4;
-        else if (result[symbol] == '6' && flag == 0)
-            yy = 5;
-        else if (result[symbol] == '7' && flag == 0)
-            yy = 6;
-        else if (result[symbol] == '8' && flag == 0)
-            yy = 7;
-
-        if (result[symbol] == 'a' && (flag == 1 || flag == 2))
-            xx1 = 0;
-        else if (result[symbol] == 'b' && (flag == 1 || flag == 2))
-            xx1 = 1;
-        else if (result[symbol] == 'c' && (flag == 1 || flag == 2))
-            xx1 = 2;
-        else if (result[symbol] == 'd' && (flag == 1 || flag == 2))
-            xx1 = 3;
-        else if (result[symbol] == 'e' && (flag == 1 || flag == 2))
-            xx1 = 4;
-        else if (result[symbol] == 'f' && (flag == 1 || flag == 2))
-            xx1 = 5;
-        else if (result[symbol] == 'g' && (flag == 1 || flag == 2))
-            xx1 = 6;
-        else if (result[symbol] == 'h' && (flag == 1 || flag == 2))
-            xx1 = 7;
-        else if (result[symbol] == '1' && (flag == 1 || flag == 2)) {
-            yy1 = 0;
-        } else if (result[symbol] == '2' && (flag == 1 || flag == 2)) {
-            yy1 = 1;
-        } else if (result[symbol] == '3' && (flag == 1 || flag == 2)) {
-            yy1 = 2;
-        } else if (result[symbol] == '4' && (flag == 1 || flag == 2)) {
-            yy1 = 3;
-        } else if (result[symbol] == '5' && (flag == 1 || flag == 2)) {
-            yy1 = 4;
-        } else if (result[symbol] == '6' && (flag == 1 || flag == 2)) {
-            yy1 = 5;
-        } else if (result[symbol] == '7' && (flag == 1 || flag == 2)) {
-            yy1 = 6;
-        } else if (result[symbol] == '8' && (flag == 1 || flag == 2)) {
-            yy1 = 7;
-        }
-    }
+    int n1 = 0, n2 = 0, n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0, n8 = 0, n9 = 0;
+    int xx, yy, xx1, yy1;
+    read_file(
+            &xx,
+            &yy,
+            &xx1,
+            &yy1,
+            &n1,
+            &n2,
+            &n3,
+            &n4,
+            &n5,
+            &n6,
+            &n7,
+            result,
+            &n8,
+            &n9);
     const int x = 4, y = 1, x1 = 4, y1 = 3;
     ASSERT_EQUAL(xx, x);
     ASSERT_EQUAL(yy, y);
@@ -98,43 +48,46 @@ CTEST(read_tt, sign_if_read)
     ASSERT_EQUAL(yy1, y1);
 }
 
-CTEST(read_tt, sign_case_read)
+CTEST(check_rule, correct_chess_hist)
+{
+    int x = 4, y = 1, x1 = 4, y1 = 3, error=0, figures = 10, n=1;
+    check_rule(x, y, x1, y1, &n, figures, 0, &error);
+    //int error=*er;
+    const int res_error = 0;
+    ASSERT_EQUAL(error, res_error);
+}
+
+CTEST(check_rule, uncorrect_chess_hist)
+{
+    int x = 4, y = 1, x1 = 4, y1 = 5, figures = 1, er = 0,n=1;
+    check_rule(x, y, x1, y1, &n, figures, 0, &er);
+    //int error=*er;
+    const int res_error = 2;
+    ASSERT_EQUAL(er, res_error);
+}
+
+CTEST(check_move, uncorrect_chess_hist)
 {
     FILE* file = fopen("test.txt", "r");
-    char result[6];
-    fgets(result, 6, file);
+    char text[40];
+    int tab[8][8];
+    start_table(tab);
+    int x = 4, y = 1, x1 = 4, y1 = 4, f = 1,n1=0,n2=0,n3=0, er = 0, m=0;
+    move_figure(m,1,x,y,x1,y1,&f,&n1,&n2,text,1,0,&n3,tab,&er,file);
+    const int res_error = 2;
     fclose(file);
+    ASSERT_EQUAL(er, res_error);
+}
 
-    int xx = 0, yy = 0, flag = 0, xx1 = 0, yy1 = 0;
-
-    for (int symbol = 0; symbol <= 5; symbol++) {
-        switch ((int)result[symbol]) {
-        case 'e':
-            if (flag == 0)
-                xx = 4;
-            else
-                xx1 = 4;
-            break;
-        case '2':
-            if (flag == 0)
-                yy = 1;
-            else
-                yy1 = 1;
-            break;
-        case '4':
-            if (flag == 0)
-                yy = 3;
-            else
-                yy1 = 3;
-            break;
-        case '-':
-            flag++;
-            break;
-        }
-    }
-    const int x = 4, y = 1, x1 = 4, y1 = 3;
-    ASSERT_EQUAL(xx, x);
-    ASSERT_EQUAL(yy, y);
-    ASSERT_EQUAL(xx1, x1);
-    ASSERT_EQUAL(yy1, y1);
+CTEST(check_move, piece_not_in_pos)
+{
+    FILE* file = fopen("test.txt", "r");
+    char text[40];
+    int tab[8][8];
+    start_table(tab);
+    int x = 4, y = 2, x1 = 4, y1 = 3, f = 1,n1=0,n2=0,n3=0, er = 0, m=0;
+    move_figure(m,1,x,y,x1,y1,&f,&n1,&n2,text,1,0,&n3,tab,&er,file);
+    const int res_error = 3;
+    fclose(file);
+    ASSERT_EQUAL(er, res_error);
 }
