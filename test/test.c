67cfae01e76b64327bfd19578b21124fa3d9d91b
Sergey Bauman <tqqqqt@users.noreply.github.com>
2022-04-14 16:00:44 +0700

    forgot format

--- test/test.c
+++ test/test.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 #include <libchessviz/check_rule.h>
+#include <libchessviz/move.h>
 #include <libchessviz/read_file.h>
 #include <libchessviz/start_table.h>
-#include <libchessviz/move.h>
 
 CTEST(read_txt, simple_read_file)
 {
@@ -50,18 +50,16 @@ CTEST(read_sign, sign_translate_xy_cord)
 
 CTEST(check_rule, correct_chess_hist)
 {
-    int x = 4, y = 1, x1 = 4, y1 = 3, error=0, figures = 10, n=1;
+    int x = 4, y = 1, x1 = 4, y1 = 3, error = 0, figures = 10, n = 1;
     check_rule(x, y, x1, y1, &n, figures, 0, &error);
-    //int error=*er;
     const int res_error = 0;
     ASSERT_EQUAL(error, res_error);
 }
 
 CTEST(check_rule, uncorrect_chess_hist)
 {
-    int x = 4, y = 1, x1 = 4, y1 = 5, figures = 1, er = 0,n=1;
+    int x = 4, y = 1, x1 = 4, y1 = 5, figures = 1, er = 0, n = 1;
     check_rule(x, y, x1, y1, &n, figures, 0, &er);
-    //int error=*er;
     const int res_error = 2;
     ASSERT_EQUAL(er, res_error);
 }
@@ -72,8 +70,10 @@ CTEST(check_move, uncorrect_chess_hist)
     char text[40];
     int tab[8][8];
     start_table(tab);
-    int x = 4, y = 1, x1 = 4, y1 = 4, f = 1,n1=0,n2=0,n3=0, er = 0, m=0;
-    move_figure(m,1,x,y,x1,y1,&f,&n1,&n2,text,1,0,&n3,tab,&er,file);
+    int x = 4, y = 1, x1 = 4, y1 = 4, f = 1, n1 = 0, n2 = 0, n3 = 0, er = 0,
+        m = 0;
+    move_figure(
+            m, 1, x, y, x1, y1, &f, &n1, &n2, text, 1, 0, &n3, tab, &er, file);
     const int res_error = 2;
     fclose(file);
     ASSERT_EQUAL(er, res_error);
@@ -85,8 +85,10 @@ CTEST(check_move, piece_not_in_pos)
     char text[40];
     int tab[8][8];
     start_table(tab);
-    int x = 4, y = 2, x1 = 4, y1 = 3, f = 1,n1=0,n2=0,n3=0, er = 0, m=0;
-    move_figure(m,1,x,y,x1,y1,&f,&n1,&n2,text,1,0,&n3,tab,&er,file);
+    int x = 4, y = 2, x1 = 4, y1 = 3, f = 1, n1 = 0, n2 = 0, n3 = 0, er = 0,
+        m = 0;
+    move_figure(
+            m, 1, x, y, x1, y1, &f, &n1, &n2, text, 1, 0, &n3, tab, &er, file);
     const int res_error = 3;
     fclose(file);
     ASSERT_EQUAL(er, res_error);
